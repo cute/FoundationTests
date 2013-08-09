fd0c8373f60903e7a719fccad86eda87e080a5bb
Daniel DeCovnick <daniel@apportable.com>
2013-08-08 17:45:53 -0700

    Finish NSInvocationTests. Adds a bunch. Removes a few that are sufficiently tested elsewhere.

--- src/NSInvocationTests.m
+++ src/NSInvocationTests.m
@@ -50,9 +50,14 @@
 {
     NSDictionary *dict = @{@"Foo": @"bar"};
     NSInvocation *inv = [NSInvocation invocationWithMethodSignature:[dict methodSignatureForSelector:@selector(objectForKey:)]];
-    [inv setTarget:dict];
     id target = [inv target];
+    testassert(target == nil);
+    [inv setTarget:dict];
+    target = [inv target];
     testassert(target == dict);
+    [inv setTarget:nil];
+    target = [inv target];
+    testassert(target == nil);
     return YES;
 }
 
@@ -83,6 +88,10 @@
     NSDictionary *dict = @{@"Foo": @"bar"};
     NSInvocation *inv = [NSInvocation invocationWithMethodSignature:[dict methodSignatureForSelector:@selector(objectForKey:)]];
     testassert([inv argumentsRetained] == NO);
+    NSUInteger rc = [dict retainCount];
+    [inv setTarget:dict];
+    testassert(rc == [dict retainCount]); // target is not retained by the invocation except if the arguments are. 
+    
     return YES;
 }
 
@@ -95,66 +104,225 @@
     testassert([inv argumentsRetained] == YES);
     [inv setTarget:dict];
     testassert(rc < [dict retainCount]);
+    
+    
+    return YES;
+}
+
+- (BOOL)testSetArgumentAtIndexBeyondBounds
+{
+    NSDictionary *dict = @{@"Foo": @"bar"};
+    NSInvocation *inv = [NSInvocation invocationWithMethodSignature:[dict methodSignatureForSelector:@selector(objectForKey:)]];
+    NSString *foo = [NSString string];
+    void (^block)(void) = ^{
+        [inv setArgument:&foo atIndex:6];
+    };
+    BOOL raised = NO;
+    @try
+    {
+        block();
+    }
+    @catch (NSException *e)
+    {
+        testassert([[e name] isEqualToString:NSInvalidArgumentException]); // why is this not an NSRangeException anyway?
+        raised = YES;
+    }
+    testassert(raised == YES);
+    
     return YES;
 }
 
 - (BOOL)testGetReturnValueUninvoked
 {
+    NSDictionary *dict = @{@"Foo": @"bar"};
+    NSInvocation *inv = [NSInvocation invocationWithMethodSignature:[dict methodSignatureForSelector:@selector(objectForKey:)]];
+    [inv setSelector:@selector(objectForKey:)];
+    [inv setTarget:dict];
+    NSString *str = @"Foo";
+    [inv setArgument:&str atIndex:2];
+    id result = nil;
+    [inv getReturnValue:&result];
+    testassert(result == nil);
     return YES;
 }
 
 - (BOOL)testGetReturnValueInvoked
 {
+    NSDictionary *dict = @{@"Foo": @"bar"};
+    NSInvocation *inv = [NSInvocation invocationWithMethodSignature:[dict methodSignatureForSelector:@selector(objectForKey:)]];
+    [inv setSelector:@selector(objectForKey:)];
+    [inv setTarget:dict];
+    NSString *str = @"Foo";
+    [inv setArgument:&str atIndex:2];
+    id result = nil;
+    [inv invoke];
+    [inv getReturnValue:&result];
+    testassert([result isEqualToString:@"bar"]);
     return YES;
 }
 
 - (BOOL)testSetReturnValueUninvoked
 {
+    NSDictionary *dict = @{@"Foo": @"bar"};
+    NSInvocation *inv = [NSInvocation invocationWithMethodSignature:[dict methodSignatureForSelector:@selector(objectForKey:)]];
+    [inv setSelector:@selector(objectForKey:)];
+    [inv setTarget:dict];
+    NSString *str = @"Foo";
+    [inv setArgument:&str atIndex:2];
+    id result = nil;
+    NSString *str2 = [dict objectForKey:@"Foo"];
+    [inv setReturnValue:&str2];
+    [inv getReturnValue:&result];
+    testassert(result == str2);
     return YES;
 }
 
 - (BOOL)testSetReturnValueInvoked
 {
-    return YES;
-}
-
-- (BOOL)testReturnValueBufferTooSmall
-{
-    return YES;
-}
-
-- (BOOL)testReturnValueBufferTooLarge
-{
-    return YES;
-}
-
-- (BOOL)testSetArgumentAtIndex
-{
-    return YES;
-}
-
-- (BOOL)testSetArgumentAtIndexWithRetainedArguments
-{
+    NSDictionary *dict = @{@"Foo": @"bar"};
+    NSInvocation *inv = [NSInvocation invocationWithMethodSignature:[dict methodSignatureForSelector:@selector(objectForKey:)]];
+    [inv setSelector:@selector(objectForKey:)];
+    [inv setTarget:dict];
+    NSString *str = @"Foo";
+    [inv setArgument:&str atIndex:2];
+    id result = nil;
+    NSString *str2 = [dict objectForKey:@"Foo"];
+    [inv setReturnValue:&str2];
+    [inv getReturnValue:&result];
+    testassert(result == str2);
+    NSString *fake = @"qux";
+    [inv setReturnValue:&fake];
+    result = nil;
+    [inv getReturnValue:&result];
+    testassert(fake == result);
     return YES;
 }
 
 - (BOOL)testGetArgumentAtIndex
 {
+    NSString *unintedString = [NSString alloc];
+    NSInvocation *inv = [NSInvocation invocationWithMethodSignature:[unintedString methodSignatureForSelector:@selector(initWithBytes:length:encoding:)]];
+    char *bytesArg = "This is the dawning of the age of Aquarius!";
+    [inv setTarget:unintedString];
+    [inv setSelector:@selector(initWithBytes:length:encoding:)];
+    [inv setArgument:&bytesArg atIndex:2];
+    NSUInteger length = strlen(bytesArg);
+    NSStringEncoding encoding = NSUTF8StringEncoding;
+    [inv setArgument:&length atIndex:3];
+    [inv setArgument:&encoding atIndex:4];
+    NSUInteger outLen;
+    [inv getArgument:&outLen atIndex:3];
+    NSStringEncoding outEnc;
+    [inv getArgument:&outEnc atIndex:4];
+    testassert(outLen == length);
+    testassert(outEnc == encoding);
+    [inv invoke]; // should still work after invocation
+    [inv getArgument:&outLen atIndex:3];
+    [inv getArgument:&outEnc atIndex:4];
+    testassert(outLen == length);
+    testassert(outEnc == encoding);
+
+    id result;
+    [inv getReturnValue:&result];
+    testassert([result isEqualToString:@"This is the dawning of the age of Aquarius!"]);
+    result = nil;
+    [inv getArgument:&result atIndex:-1];
+    testassert([result isEqualToString:@"This is the dawning of the age of Aquarius!"]);
+    [unintedString release];
     return YES;
 }
 
+- (BOOL)testCharStarBehaviorWithUnretainedArguments
+{
+    NSString *unintedString = [NSString alloc];
+    NSInvocation *inv = [NSInvocation invocationWithMethodSignature:[unintedString methodSignatureForSelector:@selector(initWithBytes:length:encoding:)]];
+    char *bytesArg = "This is the dawning of the age of Aquarius!";
+    [inv setTarget:unintedString];
+    [inv setSelector:@selector(initWithBytes:length:encoding:)];
+    [inv setArgument:&bytesArg atIndex:2];
+    NSUInteger length = strlen(bytesArg);
+    NSStringEncoding encoding = NSUTF8StringEncoding;
+    [inv setArgument:&length atIndex:3];
+    [inv setArgument:&encoding atIndex:4];
+    testassert([inv target] == unintedString);
+    testassert([inv selector] == @selector(initWithBytes:length:encoding:));
+    [inv invoke];
+    id result;
+    [inv getReturnValue:&result];
+    testassert([result isEqualToString:@"This is the dawning of the age of Aquarius!"]);
+    [unintedString release];
+    return YES;
+}
 - (BOOL)testCharStarBehaviorWithRetainedArguments
 {
+    NSString *unintedString = [NSString alloc];
+    NSInvocation *inv = [NSInvocation invocationWithMethodSignature:[unintedString methodSignatureForSelector:@selector(initWithBytes:length:encoding:)]];
+    char *bytesArg = "This is the dawning of the age of Aquarius!";
+    [inv setTarget:unintedString];
+    [inv setSelector:@selector(initWithBytes:length:encoding:)];
+    [inv setArgument:&bytesArg atIndex:2];
+    NSUInteger length = strlen(bytesArg);
+    NSStringEncoding encoding = NSUTF8StringEncoding;
+    [inv setArgument:&length atIndex:3];
+    [inv setArgument:&encoding atIndex:4];
+    testassert([inv target] == unintedString);
+    testassert([inv selector] == @selector(initWithBytes:length:encoding:));
+    [inv retainArguments];
+    [inv invoke];
+    id result;
+    [inv getReturnValue:&result];
+    testassert([result isEqualToString:@"This is the dawning of the age of Aquarius!"]);
+    [unintedString release];
+    
     return YES;
 }
 
-- (BOOL)testInvoke
+- (BOOL)testInvokeAllUnset
 {
+    NSDictionary *dict = @{@"Foo": @"bar"};
+    NSInvocation *inv = [NSInvocation invocationWithMethodSignature:[dict methodSignatureForSelector:@selector(objectForKey:)]];
+    [inv invoke]; // doesn't crash or throw an exception, just silently fails. 
+    id result = nil;
+    [inv getReturnValue:&result];
+    testassert(result == nil);
+    
     return YES;
 }
+- (BOOL)testInvokeSelAndArgsUnset
+{
+    NSDictionary *dict = @{@"Foo": @"bar"};
+    NSInvocation *inv = [NSInvocation invocationWithMethodSignature:[dict methodSignatureForSelector:@selector(objectForKey:)]];
+    [inv setTarget:dict];
+    id result = nil;
+    [inv getReturnValue:&result];
+    testassert(result == nil);
+    return YES;
 
+}
+- (BOOL)testInvokeArgsUnset
+{
+    NSDictionary *dict = @{@"Foo": @"bar"};
+    NSInvocation *inv = [NSInvocation invocationWithMethodSignature:[dict methodSignatureForSelector:@selector(objectForKey:)]];
+    [inv setSelector:@selector(objectForKey:)];
+    [inv setTarget:dict];
+    id result = nil;
+    [inv getReturnValue:&result];
+    testassert(result == nil);
+    return YES;
+
+}
 - (BOOL)testInvokeWithTarget
 {
+    NSDictionary *dict = @{@"Foo": @"bar"};
+    NSInvocation *inv = [NSInvocation invocationWithMethodSignature:[dict methodSignatureForSelector:@selector(objectForKey:)]];
+    [inv setSelector:@selector(objectForKey:)];
+    NSString *str = @"Foo";
+    [inv setArgument:&str atIndex:2];
+    id result = nil;
+    [inv invokeWithTarget:dict];
+    NSString *str2 = [dict objectForKey:@"Foo"];
+    [inv getReturnValue:&result];
+    testassert([result isEqualToString:str2]);
     return YES;
 }
 @end
