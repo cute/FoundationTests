2da3855fbe47806a700ee6957fabb0eec1d8239e
Dustin Dettmer <dustin@apportable.com>
2013-10-15 21:44:46 -0700

    First pass on the webserver.

--- src/NSURLConnectionTests.m
+++ src/NSURLConnectionTests.m
@@ -1,7 +1,22 @@
 #import "FoundationTests.h"
+#import "WebServer.h"
 
 @testcase(NSURLConnection)
 
-#warning TODO - Dustin
+- (BOOL)testSynchronous
+{
+    NSString *urlStr = [NSString stringWithFormat:@"http://%@/website.html", [WebServer.shared hostnameString]];
+    
+    NSURLRequest *req = [NSURLRequest requestWithURL:[NSURL URLWithString:urlStr]];
+    
+    NSURLResponse *response = nil;
+    NSError *error = nil;
+    
+    NSData *data = [NSURLConnection sendSynchronousRequest:req returningResponse:&response error:&error];
+    
+    NSData *otherData = [NSData dataWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"website" ofType:@"html"]];
+    
+    return [data isEqualToData:otherData];
+}
 
 @end
