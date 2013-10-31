0bb296f865845b1a7b798418bea164debb6905d6
Philippe Hausler <philippe@apportable.com>
2013-10-31 01:23:42 -0700

    Initialization/default value tests for NSURLRequest

--- src/NSURLRequestTests.m
+++ src/NSURLRequestTests.m
@@ -1,6 +1,131 @@
 #import "FoundationTests.h"
 
+@interface NSURLRequest (internal)
++ (NSTimeInterval)defaultTimeoutInterval;
++ (void)setDefaultTimeoutInterval:(NSTimeInterval)ti;
+@end
+
 @testcase(NSURLRequest)
+
+- (BOOL)testSingletonFactoryPattern
+{
+    testassert([NSURLRequest alloc] != [NSURLRequest alloc]);
+    
+    return YES;
+}
+
+- (BOOL)testDefaultTimeout
+{
+    testassert([NSURLRequest defaultTimeoutInterval] == 60.0);
+    return YES;
+}
+
+- (BOOL)testSetDefaultTimeout
+{
+    NSTimeInterval ti = [NSURLRequest defaultTimeoutInterval];
+    [NSURLRequest setDefaultTimeoutInterval:30.0];
+    
+    testassert([NSURLRequest defaultTimeoutInterval] == 30.0);
+    
+    [NSURLRequest setDefaultTimeoutInterval:ti]; // restore it for other tests
+    
+    return YES;
+}
+
+
+- (BOOL)testDefaultInit
+{
+    NSURLRequest *request = [[NSURLRequest alloc] init];
+    
+    testassert(request != nil);
+    
+    testassert([request URL] == nil);
+    
+    testassert([request cachePolicy] == NSURLRequestUseProtocolCachePolicy);
+    
+    testassert([request timeoutInterval] == 60.0);
+    
+    testassert([request mainDocumentURL] == nil);
+    
+    testassert([request networkServiceType] == NSURLNetworkServiceTypeDefault);
+    
+    testassert([request allowsCellularAccess] == YES);
+    
+    testassert([[request HTTPMethod] isEqualToString:@"GET"]);
+    
+    testassert([request allHTTPHeaderFields] == nil);
+    
+    testassert([request HTTPBody] == nil);
+    
+    testassert([request HTTPBodyStream] == nil);
+    
+    testassert([request HTTPShouldHandleCookies] == YES);
+    
+    testassert([request HTTPShouldUsePipelining] == NO);
+    
+    [request release];
+    return YES;
+}
+
+- (BOOL)testURLConstruction
+{
+    NSURL *url = [NSURL URLWithString:@"http://www.google.com"];
+    NSURLRequest *request = [[NSURLRequest alloc] initWithURL:url];
+    
+    testassert([[request URL] isEqual:url]);
+    testassert([request mainDocumentURL] == nil);
+    
+    [request release];
+    
+    return YES;
+}
+
+- (BOOL)testAdvancedConstruction
+{
+    NSURL *url = [NSURL URLWithString:@"http://www.google.com"];
+    NSURLRequest *request = [[NSURLRequest alloc] initWithURL:url cachePolicy:NSURLRequestReturnCacheDataElseLoad timeoutInterval:42.5];
+
+    testassert([request cachePolicy] == NSURLRequestReturnCacheDataElseLoad);
+
+    testassert([request timeoutInterval] == 42.5);
+
+    [request release];
+    return YES;
+}
+
+- (BOOL)testDefaultMutableInit
+{
+    NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
+    
+    testassert(request != nil);
+    
+    testassert([request URL] == nil);
+    
+    testassert([request cachePolicy] == NSURLRequestUseProtocolCachePolicy);
+    
+    testassert([request timeoutInterval] == 60.0);
+    
+    testassert([request mainDocumentURL] == nil);
+    
+    testassert([request networkServiceType] == NSURLNetworkServiceTypeDefault);
+    
+    testassert([request allowsCellularAccess] == YES);
+    
+    testassert([[request HTTPMethod] isEqualToString:@"GET"]);
+    
+    testassert([request allHTTPHeaderFields] == nil);
+    
+    testassert([request HTTPBody] == nil);
+    
+    testassert([request HTTPBodyStream] == nil);
+    
+    testassert([request HTTPShouldHandleCookies] == YES);
+    
+    testassert([request HTTPShouldUsePipelining] == NO);
+    
+    [request release];
+    return YES;
+}
 
 - (BOOL)testMutableRequest
 {
@@ -37,6 +162,33 @@
     
     testassert([req.HTTPBody isEqual:weirdData]);
     
+    return YES;
+}
+
+- (BOOL)testAllHeaderFieldsIsCopied
+{
+    NSURL *url = [NSURL URLWithString:@"http://www.google.com"];
+    NSMutableURLRequest *req = [NSMutableURLRequest requestWithURL:url];
+    NSDictionary *dict = @{@"foo": @"bar"};
+    [req setAllHTTPHeaderFields:dict];
+    testassert(dict != [req allHTTPHeaderFields]);
+    return YES;
+}
+
+- (BOOL)testAllHeaderFieldsIsCFDictionary
+{
+    NSURL *url = [NSURL URLWithString:@"http://www.google.com"];
+    NSMutableURLRequest *req = [NSMutableURLRequest requestWithURL:url];
+    NSDictionary *dict = @{@"foo": @"bar"};
+    [req setAllHTTPHeaderFields:dict];
+    testassert([[req allHTTPHeaderFields] isKindOfClass:[NSMutableDictionary class]]);
+    BOOL thrown = NO;
+    @try {
+        [(NSMutableDictionary *)[req allHTTPHeaderFields] setObject:@"baz" forKey:@"foo"];
+    } @catch(NSException *e) {
+        thrown = YES;
+    }
+    testassert(thrown);
     return YES;
 }
 
