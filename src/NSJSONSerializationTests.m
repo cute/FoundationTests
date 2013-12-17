523ae27fa44f6ca169de8e0abe6de3711344d68d
Philippe Hausler <philippe@apportable.com>
2013-12-16 17:27:56 -0800

    a slightly more extensive test of NSJSONSerialization

--- src/NSJSONSerializationTests.m
+++ src/NSJSONSerializationTests.m
@@ -55,4 +55,49 @@
     return YES;
 }
 
+- (BOOL)testGeneralDataSeralization
+{
+    NSMutableDictionary *dict = [NSMutableDictionary dictionary];
+    NSDictionary *dict2 = @{@"obj1" : @"value1", @"obj2" : @"value2"};
+    NSArray *arr1 = @[@"obj1value1", @"obj2value2", @"obj3value3"];
+    BOOL boolYes = YES;
+    int16_t int16 = 12345;
+    int32_t int32 = 2134567890;
+    uint32_t uint32 = 3124141341;
+    unsigned long long ull = 312414134131241413ull;
+    float fl = 3124134134678.13;
+    double dl = 13421331.72348729 * 1000000000000000000000000000000000000000000000000000.0;
+    long long negLong = -632414314135135234;
+    
+    dict[@"dict"] = dict2;
+    dict[@"arr"] = arr1;
+    dict[@"bool"] = @(boolYes);
+    dict[@"int16"] = @(int16);
+    dict[@"int32"] = @(int32);
+    dict[@"fl"] = @(fl);
+    dict[@"dl"] = @(dl);
+    dict[@"uint32"] = @(uint32);
+    dict[@"ull"] = @(ull);
+    dict[@"negLong"] = @(negLong);
+    
+    NSData *data = [NSJSONSerialization dataWithJSONObject:dict options:0 error:nil];
+    
+    NSDictionary *dict_back = (NSDictionary *)[NSJSONSerialization JSONObjectWithData:data
+                                                                              options:NSJSONReadingMutableContainers
+                                                                                error:nil];
+    
+    testassert(dict_back != nil);
+    testassert(![dict_back isEqualToDictionary:dict]);
+    testassert(([(NSNumber *)dict_back[@"bool"] boolValue] == boolYes));
+    testassert(([[(NSNumber *)dict_back[@"bool"] stringValue] isEqualToString:@"1"]));
+    testassert(([(NSNumber *)dict_back[@"int16"] intValue] == int16));
+    testassert(([(NSNumber *)dict_back[@"int32"] intValue] == int32));
+    testassert(([(NSNumber *)dict_back[@"uint32"] intValue] == uint32));
+    testassert(([(NSNumber *)dict_back[@"ull"] unsignedLongLongValue] == ull));
+    testassert(abs([(NSNumber *)dict_back[@"fl"] floatValue] - fl) < (fl/100000.0));
+    testassert(abs([(NSNumber *)dict_back[@"dl"] doubleValue] - dl) < (dl/1000000000.0));
+    testassert(([(NSNumber *)dict_back[@"negLong"] longLongValue] == negLong));
+    return YES;
+}
+
 @end
