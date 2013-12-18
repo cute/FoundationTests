5700191f068458f9f0ee35f4bff39c7bd03db866
Daniel DeCovnick <daniel@apportable.com>
2013-12-18 03:19:46 -0800

    add KVO dependent key and key path tests. remove -testMidCycleReregisterPartialWithContext as it seems to put KVO in a very bad state.
    remove commented-out method pending verification on iOS device. Currently, it crashes on the *next* use of KVO making it particularly hard to track down.

--- src/NSKVOTests.m
+++ src/NSKVOTests.m
@@ -96,6 +96,27 @@
 }
 @end
 
+@interface DependentObservable : Observable
+{
+    float _aFloat;
+}
+@property float aFloat;
+@end
+
+@implementation DependentObservable
++ (NSSet *)keyPathsForValuesAffectingAFloat
+{
+    return [NSSet setWithObject:@"anInt"];
+}
+
+@end
+
+@interface NestedObservable : Observable
+@property (nonatomic, strong) NestedObservable *nested;
+@end
+@implementation NestedObservable
+@end
+
 @testcase(NSKVO)
 
 #define OBSERVER_PROLOGUE(_Observable, _keyPath, _options, _context) \
@@ -325,6 +346,91 @@
         [badObservable didChangeValueForKey:@"anInt"];
         testassert([observer observationCountForKeyPath:@"anInt"] == 1); // really?
         [badObservable removeObserver:observer forKeyPath:@"anInt"];
+        return YES;
+    }
+}
+
+- (BOOL)testMidCycleReregisterWithContext
+{
+    @autoreleasepool
+    {
+        ReallyBadObservable *badObservable = [ReallyBadObservable observable];
+        Observer *observer = [Observer observer];
+        [badObservable addObserver:observer forKeyPath:@"anInt" options:0 context:NULL];
+        [badObservable willChangeValueForKey:@"anInt"];
+        [badObservable removeObserver:observer forKeyPath:@"anInt"];
+        [badObservable setAnInt:50];
+        [badObservable addObserver:observer forKeyPath:@"anInt" options:0 context:badObservable];
+        [badObservable didChangeValueForKey:@"anInt"];
+        testassert([observer observationCountForKeyPath:@"anInt"] == 0);
+        [badObservable removeObserver:observer forKeyPath:@"anInt"];
+        return YES;
+    }
+}
+
+//- (BOOL)testMidCycleReregisterPartialWithContext
+//{
+//    @autoreleasepool
+//    {
+//        ReallyBadObservable *badObservable = [ReallyBadObservable observable];
+//        Observer *observer = [Observer observer];
+//        [badObservable addObserver:observer forKeyPath:@"anInt" options:0 context:NULL];
+//        [badObservable willChangeValueForKey:@"anInt"];
+//        [badObservable removeObserver:observer forKeyPath:@"anInt"];
+//        [badObservable setAnInt:50];
+//        [badObservable addObserver:observer forKeyPath:@"anInt" options:0 context:badObservable];
+//        [badObservable addObserver:observer forKeyPath:@"anInt" options:0 context:NULL];
+//        [badObservable didChangeValueForKey:@"anInt"];
+//        testassert([observer observationCountForKeyPath:@"anInt"] == 1);
+//        [badObservable removeObserver:observer forKeyPath:@"anInt" context:badObservable];
+//        return YES;
+//        //WTF, NSKVODeallocateBreak doesn't fire here.
+//    }
+//}
+
+
+- (BOOL)testDependantKeyChange
+{
+    @autoreleasepool
+    {
+        DependentObservable *observable = [DependentObservable observable];
+        Observer *observer = [Observer observer];
+        [observable addObserver:observer forKeyPath:@"aFloat" options:0 context:NULL];
+        [observable setAnInt:5];
+        testassert([observer observationCountForKeyPath:@"aFloat"] == 1);
+        [observable removeObserver:observer forKeyPath:@"aFloat"];
+        return YES;
+    }
+}
+
+- (BOOL)testNestedObservableLeaf
+{
+    @autoreleasepool
+    {
+        NestedObservable *topLevelObservable = [NestedObservable observable];
+        NestedObservable *leafObservable = [NestedObservable observable];
+        topLevelObservable.nested = leafObservable;
+        Observer *observer = [Observer observer];
+        [topLevelObservable addObserver:observer forKeyPath:@"nested.anInt" options:0 context:NULL];
+        [leafObservable setAnInt:5];
+        testassert([observer observationCountForKeyPath:@"nested.anInt"] == 1);
+        [topLevelObservable removeObserver:observer forKeyPath:@"nested.anInt"];
+        return YES;
+    }
+}
+
+- (BOOL)testNestedObservableBranch
+{
+    @autoreleasepool
+    {
+        NestedObservable *topLevelObservable = [NestedObservable observable];
+        NestedObservable *leafObservable = [NestedObservable observable];
+        leafObservable.anInt = 5;
+        Observer *observer = [Observer observer];
+        [topLevelObservable addObserver:observer forKeyPath:@"nested.anInt" options:0 context:NULL];
+        [topLevelObservable setNested:leafObservable];
+        testassert([observer observationCountForKeyPath:@"nested.anInt"] == 1);
+        [topLevelObservable removeObserver:observer forKeyPath:@"nested.anInt"];
         return YES;
     }
 }
