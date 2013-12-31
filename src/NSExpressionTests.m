1de626d8848bb92ab80577c50f8c279fbad037b5
Philippe Hausler <philippe@apportable.com>
2013-12-31 14:38:03 -0800

    tests for a few NSExpressions

--- src/NSExpressionTests.m
+++ src/NSExpressionTests.m
@@ -34,4 +34,179 @@
     return YES;
 }
 
+- (BOOL)testSimpleMath1
+{
+    NSExpression *expression = [NSExpression expressionWithFormat:@"1+1"];
+    id value = [expression expressionValueWithObject:nil context:nil];
+    testassert([value isEqual:@(2)]);
+    return YES;
+}
+
+- (BOOL)testNewLines
+{
+    NSExpression *expression = [NSExpression expressionWithFormat:@"1\n+1"];
+    id value = [expression expressionValueWithObject:nil context:nil];
+    testassert([value isEqual:@(2)]);
+    return YES;
+}
+
+- (BOOL)testTabs
+{
+    NSExpression *expression = [NSExpression expressionWithFormat:@"1\t+1"];
+    id value = [expression expressionValueWithObject:nil context:nil];
+    testassert([value isEqual:@(2)]);
+    return YES;
+}
+
+- (BOOL)testEscaped
+{
+    BOOL thrown = NO;
+    @try {
+        NSExpression *expression = [NSExpression expressionWithFormat:@"1\\n+1"];
+        id value = [expression expressionValueWithObject:nil context:nil];
+        testassert([value isEqual:@(2)]);
+    } @catch (NSException *e) {
+        thrown = YES;
+        testassert([[e name] isEqualToString:NSInvalidArgumentException]);
+    }
+    return YES;
+}
+
+- (BOOL)testSimpleMath2
+{
+    NSExpression *expression = [NSExpression expressionWithFormat:@"0x4444+0x2222"];
+    id value = [expression expressionValueWithObject:nil context:nil];
+    testassert([value isEqual:@(0x6666)]);
+    return YES;
+}
+
+- (BOOL)testSimpleMathWithComments1
+{
+    BOOL thrown = NO;
+    @try {
+        NSExpression *expression = [NSExpression expressionWithFormat:@"1+1 /* testing a comment */"];
+        id value = [expression expressionValueWithObject:nil context:nil];
+        testassert([value isEqual:@(2)]);
+    } @catch (NSException *e) {
+        thrown = YES;
+        testassert([[e name] isEqualToString:NSInvalidArgumentException]);
+    }
+    return YES;
+}
+
+- (BOOL)testSimpleMathWithComments2
+{
+    BOOL thrown = NO;
+    @try {
+        NSExpression *expression = [NSExpression expressionWithFormat:@"1+1 // testing a comment"];
+        id value = [expression expressionValueWithObject:nil context:nil];
+        testassert([value isEqual:@(2)]);
+    } @catch (NSException *e) {
+        thrown = YES;
+        testassert([[e name] isEqualToString:NSInvalidArgumentException]);
+    }
+    return YES;
+}
+
+- (BOOL)testTrigraph
+{
+    BOOL thrown = NO;
+    @try {
+        NSExpression *expression = [NSExpression expressionWithFormat:@"1//+1"];
+        id value = [expression expressionValueWithObject:nil context:nil];
+        testassert([value isEqual:@(2)]);
+    } @catch (NSException *e) {
+        thrown = YES;
+        testassert([[e name] isEqualToString:NSInvalidArgumentException]);
+    }
+    return YES;
+}
+
+- (BOOL)testStatistics1
+{
+    NSArray *numbers = @[@1, @2, @3];
+    NSExpression *expression = [NSExpression expressionForFunction:@"sum:" arguments:@[[NSExpression expressionForConstantValue:numbers]]];
+    id value = [expression expressionValueWithObject:nil context:nil];
+    testassert([value isEqual:@(1 + 2 + 3)]);
+    return YES;
+}
+
+- (BOOL)testStatistics2
+{
+    NSArray *numbers = @[@1, @2, @3];
+    NSExpression *expression = [NSExpression expressionForFunction:@"count:" arguments:@[[NSExpression expressionForConstantValue:numbers]]];
+    id value = [expression expressionValueWithObject:nil context:nil];
+    testassert([value isEqual:@(3)]);
+    return YES;
+}
+
+- (BOOL)testStatistics3
+{
+    NSArray *numbers = @[@1, @2, @3];
+    NSExpression *expression = [NSExpression expressionForFunction:@"average:" arguments:@[[NSExpression expressionForConstantValue:numbers]]];
+    id value = [expression expressionValueWithObject:nil context:nil];
+    testassert([value isEqual:@((double)(1 + 2 + 3) / (double)3)]);
+    return YES;
+}
+
+- (BOOL)testStatistics4
+{
+    NSArray *numbers = @[@1, @2, @3];
+    NSExpression *expression = [NSExpression expressionForFunction:@"min:" arguments:@[[NSExpression expressionForConstantValue:numbers]]];
+    id value = [expression expressionValueWithObject:nil context:nil];
+    testassert([value isEqual:@(1)]);
+    return YES;
+}
+
+- (BOOL)testStatistics5
+{
+    NSArray *numbers = @[@1, @2, @3];
+    NSExpression *expression = [NSExpression expressionForFunction:@"max:" arguments:@[[NSExpression expressionForConstantValue:numbers]]];
+    id value = [expression expressionValueWithObject:nil context:nil];
+    testassert([value isEqual:@(3)]);
+    return YES;
+}
+
+- (BOOL)testStatistics6
+{
+    NSArray *numbers = @[@1, @2, @3];
+    NSExpression *expression = [NSExpression expressionForFunction:@"median:" arguments:@[[NSExpression expressionForConstantValue:numbers]]];
+    id value = [expression expressionValueWithObject:nil context:nil];
+    testassert([value isEqual:@(2)]);
+    return YES;
+}
+
+- (BOOL)testStatistics7
+{
+    NSArray *numbers = @[@1, @2, @3, @3];
+    NSExpression *expression = [NSExpression expressionForFunction:@"mode:" arguments:@[[NSExpression expressionForConstantValue:numbers]]];
+    id value = [expression expressionValueWithObject:nil context:nil];
+    testassert([value isEqual:@[@(3)]]);
+    return YES;
+}
+
+- (BOOL)testAdvancedArithmetics1
+{
+    NSExpression *expression = [NSExpression expressionForFunction:@"sqrt:" arguments:@[[NSExpression expressionForConstantValue:@9]]];
+    id value = [expression expressionValueWithObject:nil context:nil];
+    testassert([value isEqual:@(3)]);
+    return YES;
+}
+
+- (BOOL)testAdvancedArithmetics2
+{
+    NSExpression *expression = [NSExpression expressionForFunction:@"log:" arguments:@[[NSExpression expressionForConstantValue:@9]]];
+    id value = [expression expressionValueWithObject:nil context:nil];
+    testassert([value isEqual:@(log10(9))]);
+    return YES;
+}
+
+- (BOOL)testAdvancedArithmetics3
+{
+    NSExpression *expression = [NSExpression expressionForFunction:@"ln:" arguments:@[[NSExpression expressionForConstantValue:@9]]];
+    id value = [expression expressionValueWithObject:nil context:nil];
+    testassert([value isEqual:@(log(9))]);
+    return YES;
+}
+
 @end
