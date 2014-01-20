1d4d7d1845acd17744a5381c5411e055bd714726
George Kulakowski <george@apportable.com>
2014-01-20 11:00:12 -0800

    NSPurgeableData tests, based on NSData tests

---
+++ src/NSPurgeableDataTests.m
@@ -0,0 +1,432 @@
+#import "FoundationTests.h"
+
+@testcase(NSPurgeableData)
+
+test(Allocate)
+{
+    NSPurgeableData *d1 = [NSPurgeableData alloc];
+    NSPurgeableData *d2 = [NSPurgeableData alloc];
+
+    testassert(d1 != d2);
+
+    return YES;
+}
+
+test(InitWithContentsOfFileNil)
+{
+    NSPurgeableData *data = [[NSPurgeableData alloc] initWithContentsOfFile:nil];
+    testassert(data == nil);
+    return YES;
+}
+
+test(PurgeableDataWithContentsOfFileNil)
+{
+    NSPurgeableData *data = [NSPurgeableData dataWithContentsOfFile:nil];
+    testassert(data == nil);
+    return YES;
+}
+
+test(PurgeableDataWithContentsOfMappedFileNil)
+{
+    NSPurgeableData *data = [NSPurgeableData dataWithContentsOfMappedFile:nil];
+    testassert(data == nil);
+    return YES;
+}
+
+test(InitWithContentsOfURLNil)
+{
+    NSPurgeableData *data = [[NSPurgeableData alloc] initWithContentsOfURL:nil];
+    testassert(data == nil);
+    return YES;
+}
+
+test(PurgeableDataWithContentsOfURLNil)
+{
+    NSPurgeableData *data = [NSPurgeableData dataWithContentsOfURL:nil];
+    testassert(data == nil);
+    return YES;
+}
+
+test(InitWithContentsOfURLNilOptionsError)
+{
+    void (^block)() = ^{
+        [[NSPurgeableData alloc] initWithContentsOfURL:nil options:0 error:NULL];
+    };
+
+    // initWithContentsOfURL:options:error: should throw NSInvalidArgumentException
+    BOOL raised = NO;
+
+    @try {
+        block();
+    }
+    @catch (NSException *e) {
+        testassert([[e name] isEqualToString:NSInvalidArgumentException]);
+        raised = YES;
+    }
+
+    testassert(raised);
+
+    return YES;
+}
+
+test(InitWithContentsOfFileNilOptionsError)
+{
+    void (^block)() = ^{
+        [[NSPurgeableData alloc] initWithContentsOfFile:nil options:0 error:NULL];
+    };
+
+    // initWithContentsOfFile:options:error: should throw NSInvalidArgumentException
+    BOOL raised = NO;
+
+    @try {
+        block();
+    }
+    @catch (NSException *e) {
+        testassert([[e name] isEqualToString:NSInvalidArgumentException]);
+        raised = YES;
+    }
+
+    testassert(raised);
+
+    return YES;
+}
+
+test(InitWithContentsOfURLGood)
+{
+    NSPurgeableData *data = [[NSPurgeableData alloc] initWithContentsOfURL:[NSURL URLWithString:@"http://www.apportable.com/about"]];
+    testassert(data != nil);
+    [data release];
+    return YES;
+}
+
+test(InitWithContentsOfURLBad)
+{
+    NSPurgeableData *data = [[NSPurgeableData alloc] initWithContentsOfURL:[NSURL URLWithString:@"http://www.apportablexxxx.com/about"]];
+    testassert(data == nil);
+    [data release];
+    return YES;
+}
+
+test(PurgeableDataWithLength)
+{
+    NSPurgeableData *data = [NSPurgeableData dataWithLength:7];
+    testassert([data length] == 7);
+    return YES;
+}
+
+test(PurgeableDataWithPurgeableData)
+{
+    NSPurgeableData *data = [NSPurgeableData dataWithBytes:"abc" length:3];
+    NSPurgeableData *data2 = [NSPurgeableData dataWithData:data];
+    testassert([data2 length] == 3);
+    return YES;
+}
+
+test(PurgeableDataAppendBytes)
+{
+    NSPurgeableData *data = [NSPurgeableData dataWithLength:7];
+    [data appendBytes:"abc" length:3];
+    [data appendBytes:"def" length:3];
+
+    testassert([data length] == 13);
+
+    const char *bytes = [data bytes];
+
+    for (int i = 0; i < 7; i++)
+    {
+        testassert(bytes[i] == 0);
+    }
+    testassert(!strncmp("abcdef", bytes + 7, 6));
+
+    return YES;
+}
+
+test(PurgeableDataAppendBytesForcingRealloc)
+{
+    NSPurgeableData *data = [NSPurgeableData dataWithLength:2];
+    [data appendBytes:"abc" length:3];
+    [data appendBytes:"def" length:3];
+
+    testassert([data length] == 8);
+
+    const char *bytes = [data bytes];
+
+    for (int i = 0; i < 2; i++)
+    {
+        testassert(bytes[i] == 0);
+    }
+    testassert(!strncmp("abcdef", bytes + 2, 6));
+
+    return YES;
+}
+
+test(PurgeableDataReplaceBytes)
+{
+    NSPurgeableData *data = [NSPurgeableData dataWithLength:16];
+    testassert([data length] == 16);
+
+    [data replaceBytesInRange:NSMakeRange(0, 4) withBytes:"wxyz"];
+    testassert([data length] == 16);
+
+    const char *bytes = [data bytes];
+    testassert(!strncmp(bytes, "wxyz", 4));
+
+    for (int i = 4; i < 16; i++)
+    {
+        testassert(bytes[i] == 0);
+    }
+
+    return YES;
+}
+
+test(PurgeableDataResetBytes)
+{
+    NSPurgeableData *data = [NSPurgeableData dataWithLength:16];
+    testassert([data length] == 16);
+
+    [data replaceBytesInRange:NSMakeRange(0, 6) withBytes:"wxyzab"];
+    [data resetBytesInRange:NSMakeRange(4, 16)];
+    testassert([data length] == 20);
+
+    const char *bytes = [data bytes];
+    testassert(!strncmp(bytes, "wxyz", 4));
+
+    for (int i = 4; i < 20; i++)
+    {
+        testassert(bytes[i] == 0);
+    }
+
+    return YES;
+}
+
+test(RangeOfPurgeableData)
+{
+    const char *bytes = "abcdabcdbcd";
+    NSPurgeableData *data = [NSPurgeableData dataWithBytes:bytes length:strlen(bytes)];
+    NSPurgeableData *searchPurgeableData4 = [NSPurgeableData dataWithBytes:"abcd" length:4];
+    NSPurgeableData *searchPurgeableData3 = [NSPurgeableData dataWithBytes:"bcd" length:3];
+    NSPurgeableData *searchPurgeableData0 = [NSPurgeableData data];
+    NSRange range;
+
+    range = [data rangeOfData:searchPurgeableData0 options:0 range:NSMakeRange(0, [data length])];
+    testassert(range.location == NSNotFound);
+    testassert(range.length == 0);
+
+    range = [data rangeOfData:searchPurgeableData4 options:0 range:NSMakeRange(0, [data length])];
+    testassert(range.location == 0);
+    testassert(range.length == 4);
+
+    range = [data rangeOfData:searchPurgeableData4 options:0 range:NSMakeRange(1, [data length] - 1)];
+    testassert(range.location == 4);
+    testassert(range.length == 4);
+
+    range = [data rangeOfData:searchPurgeableData4 options:NSDataSearchAnchored range:NSMakeRange(1, [data length] - 1)];
+    testassert(range.location == NSNotFound);
+    testassert(range.length == 0);
+
+    range = [data rangeOfData:searchPurgeableData4 options:NSDataSearchBackwards range:NSMakeRange(0, [data length])];
+    testassert(range.location == 4);
+    testassert(range.length == 4);
+
+    range = [data rangeOfData:searchPurgeableData4 options:NSDataSearchAnchored|NSDataSearchBackwards range:NSMakeRange(1, [data length] - 1)];
+    testassert(range.location == NSNotFound);
+    testassert(range.length == 0);
+
+    range = [data rangeOfData:searchPurgeableData3 options:0 range:NSMakeRange(0, [data length])];
+    testassert(range.location == 1);
+    testassert(range.length == 3);
+
+    range = [data rangeOfData:searchPurgeableData3 options:0 range:NSMakeRange(1, [data length] - 1)];
+    testassert(range.location == 1);
+    testassert(range.length == 3);
+
+    range = [data rangeOfData:searchPurgeableData3 options:0 range:NSMakeRange(1, 2)];
+    testassert(range.location == NSNotFound);
+    testassert(range.length == 0);
+
+    range = [data rangeOfData:searchPurgeableData3 options:NSDataSearchAnchored range:NSMakeRange(1, [data length] - 1)];
+    testassert(range.location == 1);
+    testassert(range.length == 3);
+
+    range = [data rangeOfData:searchPurgeableData3 options:NSDataSearchBackwards range:NSMakeRange(0, [data length])];
+    testassert(range.location == 8);
+    testassert(range.length == 3);
+
+    range = [data rangeOfData:searchPurgeableData3 options:NSDataSearchAnchored|NSDataSearchBackwards range:NSMakeRange(1, [data length] - 1)];
+    testassert(range.location == 8);
+    testassert(range.length == 3);
+
+    return YES;
+}
+
+test(NSPurgeableData_writeToFileAtomicallyYes)
+{
+    const char bytes[] = {"foo"};
+    NSPurgeableData *data = [NSPurgeableData dataWithBytes:bytes length:4];
+
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *documentsDirectory = [paths objectAtIndex:0];
+    NSString *filePath = [documentsDirectory stringByAppendingPathComponent:@"NSPurgeableDataOutput0"];
+
+    BOOL result = [data writeToFile:filePath atomically:YES];
+    testassert(result);
+
+    NSPurgeableData *data2 = [NSPurgeableData dataWithContentsOfFile:filePath];
+    testassert([data2 isEqualToData:data]);
+
+    return YES;
+}
+
+test(NSPurgeableData_writeToFileAtomically_withNilValue)
+{
+    const char bytes[] = {"foo"};
+    NSPurgeableData *data = [NSPurgeableData dataWithBytes:bytes length:4];
+    BOOL result = [data writeToFile:nil atomically:YES];
+    testassert(!result);
+    return YES;
+}
+
+test(NSPurgeableData_writeToFileAtomically_withEmptyValue)
+{
+    NSPurgeableData *data = [NSPurgeableData data];
+
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *documentsDirectory = [paths objectAtIndex:0];
+    NSString *filePath = [documentsDirectory stringByAppendingPathComponent:@"DictionaryTest1"];
+
+    BOOL result = [data writeToFile:filePath atomically:YES];
+    testassert(result);
+
+    NSPurgeableData *data2 = [NSPurgeableData dataWithContentsOfFile:filePath];
+    testassert([data2 isEqualToData:data]);
+
+    return YES;
+}
+
+test(NSPurgeableData_writeToURLAtomicallyYes)
+{
+    const char bytes[] = {"foo"};
+    NSPurgeableData *data = [NSPurgeableData dataWithBytes:bytes length:4];
+
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *documentsDirectory = [paths objectAtIndex:0];
+    NSString *filePath = [documentsDirectory stringByAppendingPathComponent:@"NSPurgeableDataOutput0"];
+
+    BOOL result = [data writeToURL:[NSURL fileURLWithPath:filePath] atomically:YES];
+    testassert(result);
+
+    NSPurgeableData *data2 = [NSPurgeableData dataWithContentsOfFile:filePath];
+    testassert([data2 isEqualToData:data]);
+
+    return YES;
+}
+
+test(NSPurgeableData_writeToURLAtomically_withNilValue)
+{
+    const char bytes[] = {"foo"};
+    NSPurgeableData *data = [NSPurgeableData dataWithBytes:bytes length:4];
+    BOOL result = [data writeToURL:nil atomically:YES];
+    testassert(!result);
+    return YES;
+}
+
+test(NSPurgeableData_writeToURLAtomically_withEmptyValue)
+{
+    NSPurgeableData *data = [NSPurgeableData data];
+
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *documentsDirectory = [paths objectAtIndex:0];
+    NSString *filePath = [documentsDirectory stringByAppendingPathComponent:@"DictionaryTest1"];
+
+    BOOL result = [data writeToURL:[NSURL fileURLWithPath:filePath] atomically:YES];
+    testassert(result);
+
+    NSPurgeableData *data2 = [NSPurgeableData dataWithContentsOfFile:filePath];
+    testassert([data2 isEqualToData:data]);
+
+    return YES;
+}
+
+test(NSPurgeableData_writeToFile_options_error)
+{
+    const char bytes[] = {"foo"};
+    NSPurgeableData *data = [NSPurgeableData dataWithBytes:bytes length:4];
+
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *documentsDirectory = [paths objectAtIndex:0];
+    NSString *filePath = [documentsDirectory stringByAppendingPathComponent:@"NSPurgeableDataOutput100"];
+
+    NSError *error = nil;
+    BOOL result = [data writeToFile:filePath options:0 error:&error];
+    testassert(result);
+
+    NSPurgeableData *data2 = [NSPurgeableData dataWithContentsOfFile:filePath];
+    testassert([data2 isEqualToData:data]);
+
+    return YES;
+}
+
+test(NSPurgeableData_writeToNilFile_options_error)
+{
+    const char bytes[] = {"foo"};
+    NSPurgeableData *data = [NSPurgeableData dataWithBytes:bytes length:4];
+
+    NSError *error = nil;
+
+    BOOL exception = NO;
+    @try {
+        [data writeToFile:nil options:0 error:&error];
+    }
+    @catch (NSException *e) {
+        exception = YES;
+        testassert([[e name] isEqualToString:NSInvalidArgumentException]);
+    }
+
+    testassert(exception);
+    testassert(error == nil);
+
+    return YES;
+}
+
+test(NSPurgeableData_writeToURL_options_error)
+{
+    const char bytes[] = {"foo"};
+    NSPurgeableData *data = [NSPurgeableData dataWithBytes:bytes length:4];
+
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *documentsDirectory = [paths objectAtIndex:0];
+    NSString *filePath = [documentsDirectory stringByAppendingPathComponent:@"NSPurgeableDataOutput100"];
+
+    NSError *error = nil;
+    BOOL result = [data writeToURL:[NSURL fileURLWithPath:filePath] options:0 error:&error];
+    testassert(result);
+
+    NSPurgeableData *data2 = [NSPurgeableData dataWithContentsOfFile:filePath];
+    testassert([data2 isEqualToData:data]);
+
+    return YES;
+}
+
+test(NSPurgeableData_writeToNilURL_options_error)
+{
+    const char bytes[] = {"foo"};
+    NSPurgeableData *data = [NSPurgeableData dataWithBytes:bytes length:4];
+
+    NSError *error = nil;
+
+    BOOL exception = NO;
+    @try {
+        [data writeToURL:nil options:0 error:&error];
+    }
+    @catch (NSException *e) {
+        exception = YES;
+        testassert([[e name] isEqualToString:NSInvalidArgumentException]);
+    }
+
+    testassert(exception);
+    testassert(error == nil);
+
+    return YES;
+}
+
+@end
